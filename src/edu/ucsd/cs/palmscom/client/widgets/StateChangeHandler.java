84ae7663340dd74c61ce30a1593698de6d1275ef
Egil Hansen <egil@assimilated.dk>
2011-11-27 21:49:12 -0800

    clean up

--- src/edu/ucsd/cs/palmscom/client/widgets/StateChangeHandler.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsd.cs.palmscom.client.widgets;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface StateChangeHandler extends EventHandler {
-	void onStateChange(StateChangeEvent event);
-}

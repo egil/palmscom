21264bd1c93c508c288dc9b6bb536beaa55ea4b9
Egil Hansen <egil@assimilated.dk>
2011-12-16 12:31:48 -0800

    update to gitignore

--- src/edu/ucsd/cs/palmscom/shared/PalmscomServiceAsync.java
+++ src/edu/ucsd/cs/palmscom/shared/PalmscomServiceAsync.java
@@ -2,26 +2,16 @@ package edu.ucsd.cs.palmscom.shared;
 
 import java.util.Date;
 import java.util.List;
-
-import com.google.gwt.i18n.client.Dictionary;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface PalmscomServiceAsync {
 
 	void getMessages(int limit, AsyncCallback<List<Message>> callback);
-
 	void getMessages(Date to, AsyncCallback<List<Message>> callback);
-
 	void getMessages(Date from, Date to, AsyncCallback<List<Message>> callback);
-
 	void getOnlineUsers(AsyncCallback<List<User>> callback);
-
 	void search(String query, AsyncCallback<List<Message>> callback);
-
 	void sendMessage(Message msg, AsyncCallback<Void> callback);
-
 	void getUserSettings(AsyncCallback<Settings> callback);
-
 	void saveUserSettings(Settings settings, AsyncCallback<Void> callback);
-
 }
